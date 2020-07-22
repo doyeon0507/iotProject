4a88fad8d6176c90d162e08ddd05ee5f3c46985a
doyeon0507 <66464250+doyeon0507@users.noreply.github.com>
2020-07-22 17:12:53 +0900

    Update pir.py

--- pir.py
+++ pir.py
@@ -1,42 +1,64 @@
-#!/user/bin/python
+#!/usr/bin/python
 
+import Adafruit_DHT
 import time
-import RPi.GPIO as GPIO
 import requests, json
+import RPi.GPIO as GPIO
 from influxdb import InfluxDBClient as influxdb
-
-GPIO.setmode(GPIO,BCM)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
-def interrupt_firde(channel):
-    print("interrput Fired")
-    a=5
-    print(a)
-
-GPIO.add_event_detect(4, GPIO.FALLING, callback=interrupt_firde)
-
-while(True):
-    time.sleep(1)
-    a=1
+def interrupt_fired(channel):
+    a = 5
     data = [{
         'measurement':'pir',
         'tags':{
-            'VisionUni':'2410',
-            },
-            'fields':{
+            'visionUV':'2410', 
+        },
+        'fields':{
             'pir':a,
-            }
-         }]
-    client = None
- try:
-     client = influxdb('localhost',8086,'root','root','pir')
- except Exception as e:
-             print "Exception" + str(e)
-           if client is not None:
-                try:
-                    client.write_points(data)
-                except Exception as e:
-                    print "Exception write " +str(e)
-                finally:
-                    client.close()
-             print("running influxdb OK")
+        }
+    }]
+    client=None
+    try:
+        client = influxdb('localhost',8086,'root','root','pir')
+    except Exception as e:
+        print "Exception" + str(e)
+    if client is not None:
+        try:
+            client.write_points(data)
+        except Exception as e:
+            print "Exception write " + str(e)
+        finally:
+            client.close()
+    print("interrupt Fired") 
+    print(a)
+
+
+GPIO.add_event_detect(4, GPIO.FALLING, callback=interrupt_fired)
+
+while True:
+    time.sleep(1)
+    a = 1
+    data = [{
+        'measurement':'pir',
+        'tags':{
+            'visionUV':'2410',
+         },
+        'fields':{
+            'pir':a,
+         }
+    }]
+    client=None
+    try:
+        client = influxdb('localhost',8086,'root','root','pir')
+    except Exception as e:
+        print "Exception" + str(e)
+    if client is not None:
+        try:
+            client.write_points(data)
+        except Exception as e:
+            print "Exception write "+str(e)
+        finally:
+            client.close()
+    print("runnig influxdb OK")
